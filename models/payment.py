4f9c4d97a8187399921afbef56c5106210859544
Jason Goh <jasongoh@Jasons-MacBook-Pro-2.local>
2019-05-06 12:11:41 +0800

    cancelled out paym,ent

--- models/payment.py
+++
@@ -1,8 +0,0 @@
-import peewee as pw
-from models.base_model import BaseModel
-from models.user import User
-
-
-class Payment(BaseModel):
-    amount = pw.DecimalField()
-    payment_nonce = pw.TextField()
--- quando_api/__init__.py
+++ quando_api/__init__.py
@@ -4,16 +4,11 @@ from flask_cors import CORS
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 ## API Routes ##
-from quando_api.blueprints.users.views import users_api_blueprint
-from quando_api.blueprints.login.views import login_api_blueprint
-from quando_api.blueprints.payment.views import payment_api_blueprint
 from quando_api.blueprints.sessions.views import sessions_api_blueprint
+from quando_api.blueprints.users.views import users_api_blueprint
 
-
-app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
-app.register_blueprint(login_api_blueprint, url_prefix='/api/v1/login')
-app.register_blueprint(payment_api_blueprint, url_prefix='/api/v1/payment')
 app.register_blueprint(sessions_api_blueprint, url_prefix='/api/v1/sessions')
+app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
 
 
 
